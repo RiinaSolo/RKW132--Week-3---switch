9af411c5423d8cf5bd828ace43c2fcc0d8071cb4
Riina Solomonova <riinasolomonova@Riinas-MacBook-Pro.local>
2024-11-29 14:16:44 +0200

    add week3 to code

---
+++ SwtichDayoftheWeek/Program.cs
@@ -0,0 +1,31 @@
+﻿int weekDay = 10;//Convert.ToInt32(DateTime.Now.DayOfWeek);
+
+switch (weekDay) //väärtus, mida kontrollitakse
+{
+    case 0: //case 0: == if(weekday == 0 )
+        Console.WriteLine("It's Sunday");
+        break;
+    case 1: //case 0: == if(weekday == 0 )
+        Console.WriteLine("It's Monday");
+        break;
+    case 2: //case 0: == if(weekday == 0 )
+        Console.WriteLine("It's Tuesday");
+        break;
+    case 3: //case 0: == if(weekday == 0 )
+        Console.WriteLine("It's Wednesday");
+        break;
+    case 4: //case 0: == if(weekday == 0 )
+        Console.WriteLine("It's Thursday");
+        break;
+    case 5: //case 0: == if(weekday == 0 )
+        Console.WriteLine("It's Friday");
+        break;
+    case 6: //case 0: == if(weekday == 0 )
+        Console.WriteLine("It's Saturday");
+        break;
+    default:
+        Console.WriteLine("Opps, something went wrong, go on vacation");
+        break;
+}
+
+Console.WriteLine("Have a nice day!");
